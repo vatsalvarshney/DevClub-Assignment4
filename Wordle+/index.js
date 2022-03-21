56a2ad1574218d588a0d4649e529a2f1f8ff3c62
Vatsal Varshney <vatsalvar1211@gmail.com>
2022-03-21 23:13:56 +0530

    Task-2 Completed

--- Wordle+/index.js
+++ Wordle+/index.js
@@ -1,13 +1,48 @@
 const http = require("http");
 
-const SECRET = "CIGAR"; // You can set any word as the secret answer
+let SECRET = "NASTY";
 
 function myFunction(req, res) {
-	// console.log({req}); // You can uncomment this to see the request object
 	console.log(req.url);
 
-	const GUESS = ""; // Write logic to parse the word which the user guessed from the URL string
-	const feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
+	let newreq = req.url.split("?q=");
+
+	let feedback = ""
+	let GUESS = ""
+	
+	if (newreq.length>1){
+		GUESS = newreq[1];
+		if (GUESS.length!=SECRET.length){
+			feedback= "Error: length of guess should be "+SECRET.length.toString();
+		}else{
+			const guessarr = GUESS.split('');
+			const secretarr = SECRET.split('');
+		
+			let secretfreq ={};
+			for (let i = 0; i < secretarr.length; i++) {
+				let freq = 0;
+				for (let j = 0; j < secretarr.length; j++) {
+					if (secretarr[i]==secretarr[j]){freq++};
+				}
+				secretfreq[secretarr[i]]=freq;
+			}
+		
+			let appearences = {};
+				
+			for (let i=0; i<guessarr.length; i++) {
+				appearences[guessarr[i]] = appearences[guessarr[i]] ? appearences[guessarr[i]] + 1 : 1;
+				if (secretarr.includes(guessarr[i]) & appearences[guessarr[i]]<=secretfreq[guessarr[i]]){
+					if (guessarr[i]==secretarr[i]){
+						feedback+='g';
+					}else{
+						feedback+='y';
+					}
+				}else{
+					feedback+='b';
+				}
+			}
+		}
+	}
 
 	res.write(feedback);
 	res.end();
