04071d78e8e354a238c03d3a69dc161560fb217f
Vatsal Varshney <vatsalvar1211@gmail.com>
2022-03-21 13:12:18 +0530

    Task-1 Completed

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -1,10 +1,22 @@
 console.log("Moodle+ successfully loaded!");
-const login_element = document.querySelector(""); // Fill the selector for the login element in ""
+const login_element = document.querySelector("#login");
 let login_text = login_element.innerText;
 
-let question = ""; // Use split and array operations on the login_text string to extract the question
+let question = login_text.split("\n")[3].split(" ");
 
-let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
+let answer = "";
 
-const captcha_input_element = document.querySelector(""); // Fill the selector for the captcha input element in ""
+if (question[1]=="add") {
+    let ans = Number(question[2])+Number(question[4]);
+    answer = ans.toString();
+} else if (question[1]=="subtract") {
+    let ans = Number(question[2])-Number(question[4]);
+    answer = ans.toString();
+} else if (question[2]=="first") {
+    answer = question[4].toString();
+} else {
+    answer =question[6].toString();
+}
+
+const captcha_input_element = document.querySelector("#valuepkg3");
 captcha_input_element.value = answer;
